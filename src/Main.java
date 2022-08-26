9193d2ccbce5aa6b1b89c0f733c6812eab2a79a9
AbdelrhamanTareq <abdo_tarek01@yahoo.com>
2022-08-26 17:10:31 +0200

    stack balanced expression example

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,45 @@ public class Main {
         s1.pop();
         System.out.println(s1.getTop());
         s1.print();
+        System.out.println("---------------------------");
+        String exp = "((2*3))+{3*5}}";
+        boolean areBalanced = areBalanced(exp);
+        System.out.println(areBalanced);
+        System.out.println("---------------------------");
+        String exp2 = "{((2*3))+{3*5}}";
+        boolean areBalanced2 = areBalanced(exp2);
+        System.out.println(areBalanced2);
 
 
+    }
+
+
+
+    static boolean arePair(char open, char close){
+        if (open == '(' && close == ')'){
+            return true;
+        } else if (open == '{' && close == '}') {
+            return true;
+        } else if (open == '[' && close == ']') {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean areBalanced(String expression){
+        LinkedStack<Character> s = new LinkedStack<Character>();
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '(' || expression.charAt(i) == '{' || expression.charAt(i) == '['){
+                s.push(expression.charAt(i));
+            } else if (expression.charAt(i) == ')' || expression.charAt(i)== '}' || expression.charAt(i)== ']') {
+                if (s.isEmpty() || ! arePair((Character) s.getTop(), expression.charAt(i))) {
+                    return  false;
+                }
+                else {
+                    s.pop();
+                }
+            }
+        }
+        return s.isEmpty() ? true : false;
     }
 }
